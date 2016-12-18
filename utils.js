ff19c843923ad72d0af962dbe6e4029689d1c03a
alight <limingwxm@126.com>
2016-12-18 17:23:32 +0800

    deep copy

--- utils.js
+++ utils.js
@@ -20,9 +20,33 @@ var unique = function (arr) {
 };
 
 
-
+var a = [1,2,3,{x:1}];
+//slice做的是浅拷贝, concat做的也是浅拷贝
+var b = a.slice(0);
+var c = a.concat([4]);
+//parse&stringify才能做深拷贝
+var d = JSON.parse(JSON.stringify(a));
 
 //////////////////////////////////////////////////////////
 //对象类处理函数
 //////////////////////////////////////////////////////////
+
+/**
+ * 对象深拷贝
+ * @param source
+ * @returns {{}}
+ */
+var deepCody = function (source) {
+    var result = {};
+    for (var key in source) {
+        result[key] = typeof source[key] === 'object' ? deepCody(source[key]) : source[key];
+    }
+    return result;
+};
+
+//对象合并
+//Object.assign
+//$.extend() 第一个参数 标示是否深拷贝
+
+
 
