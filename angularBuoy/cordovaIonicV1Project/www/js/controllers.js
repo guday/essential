f7f724407d5af4582fc79c4d75395860218d4714
alight <limingwxm@126.com>
2017-01-19 20:19:46 +0800

    在ionic1的demo基础上，写模拟程序

--- angularBuoy/cordovaIonicV1Project/www/js/controllers.js
+++ angularBuoy/cordovaIonicV1Project/www/js/controllers.js
@@ -1,28 +1,103 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+  .controller('LogCtrl', function ($scope) {
+    $scope.data = {};
+    $scope.autoLog = true;
+    $scope.autoFullLog = false;
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    var logTimer, fullLogTimer;
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+    /**
+     * 初始化
+     */
+    var initilize = function () {
+      triggerLog({type: "log", enable: $scope.autoLog});
+      triggerLog({type: "fullLog", enable: $scope.autoFullLog})
+    };
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    /**
+     * 打印log函数
+     * @param logType
+     * @param content
+     */
+    var logFn = function (logType, content) {
+      var fnArr = [];
+      if (logType == "log") {
+        fnArr = ["log"];
+      } else if (logType == "fullLog") {
+        fnArr = [
+          "log",
+          "info",
+          "warn",
+          "error"
+        ]
+      } else if (logType == "userLog") {
+        console.log("用户执行log:" + content || "");
+        return;
+      }
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+      for (var i in fnArr) {
+        var fnName = fnArr[i];
+        console[fnName] && console[fnName]("自动" + logType +
+          ";类型:" + fnName +
+          ";时间:" + (new Date()).toLocaleTimeString())
+      }
+    };
+
+    /**
+     * 触发 log打印
+     * @param param
+     */
+    var triggerLog = function (param) {
+      var type = param.type;
+      var enable = param.enable;
+
+      if (type == "log") {
+        if (enable) {
+          logTimer = setInterval(function () {
+            logFn(type)
+          }, 1000)
+        } else {
+          clearInterval(logTimer);
+          logTimer = null;
+        }
+      } else if (type == "fullLog") {
+        if (enable) {
+          fullLogTimer = setInterval(function () {
+            logFn(type)
+          }, 1000)
+        } else {
+          clearInterval(fullLogTimer);
+          fullLogTimer = null;
+        }
+      } else {
+        logFn(type, param.content || "");
+      }
+    };
+
+
+    $scope.onClickLog = function () {
+      $scope.autoLog = !$scope.autoLog;
+      triggerLog({type: "log", enable: $scope.autoLog})
+    };
+
+    $scope.onClickFullLog = function () {
+      $scope.autoFullLog = !$scope.autoFullLog;
+      triggerLog({type: "fullLog", enable: $scope.autoFullLog})
+    };
+
+    $scope.onClickUserLog = function () {
+      triggerLog({type: "userLog", content: $scope.data.userLog})
+    };
+
+    initilize();
+
+  })
+
+  .controller('NetCtrl', function ($scope, Chats) {
+
+  })
+
+  .controller('LocalstorageCtrl', function ($scope) {
+
+  });
